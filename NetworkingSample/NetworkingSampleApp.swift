97314b4442c6e4bd32852e3343778c6f83fbc357
e-V01 <91158407+e-V01@users.noreply.github.com>
2023-11-15 14:02:52 +0200

    Initial Commit

---
+++ NetworkingSample/NetworkingSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingSampleApp.swift
+//  NetworkingSample
+//
+//  Created by Y K on 15.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
