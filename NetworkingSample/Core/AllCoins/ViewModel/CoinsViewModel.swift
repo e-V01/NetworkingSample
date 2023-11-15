d8aec3ec0cd77c609668fd976dc67c8a7a360278
e-V01 <91158407+e-V01@users.noreply.github.com>
2023-11-15 17:32:09 +0200

    ViewModel jazz

--- NetworkingSample/Core/AllCoins/ViewModel/CoinsViewModel.swift
+++ NetworkingSample/Core/AllCoins/ViewModel/CoinsViewModel.swift
@@ -14,20 +14,33 @@ class CoinsViewModel: ObservableObject {
     @Published var price = ""
     
     init() {
-        fetchPrice()
+        fetchPrice(coin: "ethereum")
     }
     
-    func fetchPrice() {
-        let urlString = "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd"
+    func fetchPrice(coin: String) {
+        let urlString = "https://api.coingecko.com/api/v3/simple/price?ids=\(coin)&vs_currencies=usd"
         guard let url = URL(string: urlString) else { return }
-        print("Fetching price ...")
+        print("Fetching price ...") // 1 seq
         
         URLSession.shared.dataTask(with: url) { data, response, error in
-            print("Did receive data \(data)")
+            print("Did receive data \(data)") // 3rd seq
+            
+            guard let data = data else { return }
+            guard let jsonObject = try? JSONSerialization.jsonObject(with: data) as? [String: Any] else { return }
+
+            guard let value = jsonObject[coin] as? [String: Double] else { return }
+            guard let price = value["usd"] else { return }
+            DispatchQueue.main.async {
+                self.coin = coin.capitalized
+                self.price = "$\(price)"
+            }
+            
+            
+//            print("JSON \(jsonObject)")
         }
         .resume()
         // necessary
-        print("Did reach end of functon ...")
+        print("Did reach end of functon ...") // 2nd seq
     }
 
 }
