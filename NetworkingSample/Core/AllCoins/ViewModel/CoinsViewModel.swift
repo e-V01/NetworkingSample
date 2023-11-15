8af91cc81529d721a7e12c1dc07aed4384b38815
e-V01 <91158407+e-V01@users.noreply.github.com>
2023-11-15 18:15:31 +0200

    logic of thread and main thread

--- NetworkingSample/Core/AllCoins/ViewModel/CoinsViewModel.swift
+++ NetworkingSample/Core/AllCoins/ViewModel/CoinsViewModel.swift
@@ -15,14 +15,19 @@ class CoinsViewModel: ObservableObject {
     
     init() {
         fetchPrice(coin: "ethereum")
+        fetchPrice(coin: "bitcoin")
+
     }
     
     func fetchPrice(coin: String) {
+        print(Thread.current)
+        
         let urlString = "https://api.coingecko.com/api/v3/simple/price?ids=\(coin)&vs_currencies=usd"
         guard let url = URL(string: urlString) else { return }
         print("Fetching price ...") // 1 seq
         
         URLSession.shared.dataTask(with: url) { data, response, error in
+            print(Thread.current)
             print("Did receive data \(data)") // 3rd seq
             
             guard let data = data else { return }
@@ -30,7 +35,9 @@ class CoinsViewModel: ObservableObject {
 
             guard let value = jsonObject[coin] as? [String: Double] else { return }
             guard let price = value["usd"] else { return }
+            
             DispatchQueue.main.async {
+                print(Thread.current)
                 self.coin = coin.capitalized
                 self.price = "$\(price)"
             }
